ca203b14fb5d3c877516984c33e55b6a54e7a344
Ben Leynen <leynenben@gmail.com>
2018-02-23 15:58:31 +0100

    Fixed script

--- scripts/installWebserver.sh
+++ scripts/installWebserver.sh
@@ -10,14 +10,20 @@ echo 'export PS1="[\[\e[31m\]\u\[\e[39m\]@\[\e[31m\]\h \[\e[34m\]\W\[\e[39m\]]$ 
 # Update
 yum update -y
 # Install httpd
-yum install -y httpd php
+yum install -y httpd php git
 
 # Git clone the repo
-git clone ~/azure-centos-webserver
+git clone https://github.com/leynebe/azure-centos-webserver ~/azure-centos-webserver
 # Move site files to web dir
 mv ~/azure-centos-webserver/site-files/* /var/www/html/
 # Write name to file
 echo -e "$NAME" > /var/www/html/name.txt
+
+# Change owner of web dir
+chown -R apache:apache /var/www/html
+
+# Fix SELinux context
+restorecon -Rv /var/www/html
 
 # Start service
 systemctl restart httpd && systemctl enable httpd
