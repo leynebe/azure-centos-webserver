11e56d3933f50d8a7a82af2d53b8515b90e8b9a7
Ben Leynen <leynenben@gmail.com>
2018-02-23 15:13:31 +0100

    Added security group port

--- scripts/installWebserver.sh
+++ scripts/installWebserver.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Parameters
 NAME=$1;
+
+# Change prompts
+echo 'export PS1="[\[\e[32m\]\u\[\e[39m\]@\[\e[32m\]\h \[\e[34m\]\W\[\e[39m\]]$ "' >> /home/centos/.bashrc
+echo 'export PS1="[\[\e[31m\]\u\[\e[39m\]@\[\e[31m\]\h \[\e[34m\]\W\[\e[39m\]]$ "' >> /root/.bashrc
 
 # Update
 yum update -y
