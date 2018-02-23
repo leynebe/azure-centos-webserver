8e7c8e98908c430702e52be09b6747d1c56492fb
Ben Leynen <leynenben@gmail.com>
2018-02-23 15:31:45 +0100

    Added site files

---
+++ site-files/index.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>Hello_<?php echo file_get_contents("name.txt"); ?>!</title>
+  </head>
+  <body>
+    <h1>Hello_<?php echo file_get_contents("name.txt"); ?>!</h1>
+  </body>
+</html>
